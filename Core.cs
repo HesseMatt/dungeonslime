45d0f63398ad01ecd2623247b18a1354c9a5b7cf
Matthias Hesse <info@matt-hesse.de>
2026-09-09 17:56:42 +0200

    Chapter 15

--- Core.cs
+++ Core.cs
@@ -46,6 +46,11 @@ public class Core : Game
     public static bool ExitOnEscape { get; set; }
 
     /// <summary>
+    /// Gets a reference to the audio control system.
+    /// </summary>
+    public static AudioController Audio { get; private set; }
+
+    /// <summary>
     /// Creates a new Core instance.
     /// </summary>
     /// <param name="title">The title to display in the title bar of the game window.</param>
@@ -104,12 +109,26 @@ public class Core : Game
 
         // Create a new input manager.
         Input = new InputManager();
+
+        // Create a new audio controller.
+        Audio = new AudioController();
+    }
+
+    protected override void UnloadContent()
+    {
+        // Dispose of the audio controller.
+        Audio.Dispose();
+
+        base.UnloadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
         // Update the input manager.
         Input.Update(gameTime);
+
+        // Update the audio controller.
+        Audio.Update();
 
         if (ExitOnEscape && Input.Keyboard.WasKeyJustPressed(Keys.Escape))
         {
--- Game1.cs
+++ Game1.cs
@@ -39,6 +39,9 @@ public class Game1 : Core
     // The sound effect to play when the slime eats a bat.
     private SoundEffect _collectSoundEffect;
 
+    // The background theme song
+    private Song _themeSong;
+
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
 
@@ -51,22 +54,25 @@ public class Game1 : Core
         Rectangle screenBounds = GraphicsDevice.PresentationParameters.Bounds;
 
         _roomBounds = new Rectangle(
-             (int)_tilemap.TileWidth,
-             (int)_tilemap.TileHeight,
-             screenBounds.Width - (int)_tilemap.TileWidth * 2,
-             screenBounds.Height - (int)_tilemap.TileHeight * 2
-         );
+            (int)_tilemap.TileWidth,
+            (int)_tilemap.TileHeight,
+            screenBounds.Width - (int)_tilemap.TileWidth * 2,
+            screenBounds.Height - (int)_tilemap.TileHeight * 2
+        );
 
         // Initial slime position will be the center tile of the tile map.
         int centerRow = _tilemap.Rows / 2;
         int centerColumn = _tilemap.Columns / 2;
         _slimePosition = new Vector2(centerColumn * _tilemap.TileWidth, centerRow * _tilemap.TileHeight);
 
-        // Initial bat position will be in the top left corner of the room
+        // Initial bat position will the in the top left corner of the room.
         _batPosition = new Vector2(_roomBounds.Left, _roomBounds.Top);
 
         // Assign the initial random velocity to the bat.
         AssignRandomBatVelocity();
+
+        // Start playing the background music.
+        Audio.PlaySong(_themeSong);
     }
 
     protected override void LoadContent()
@@ -86,28 +92,14 @@ public class Game1 : Core
         _tilemap = Tilemap.FromFile(Content, "images/tilemap-definition.xml");
         _tilemap.Scale = new Vector2(4.0f, 4.0f);
 
-        // Load the bounce sound effect
+        // Load the bounce sound effect.
         _bounceSoundEffect = Content.Load<SoundEffect>("audio/bounce");
 
-        // Load the collect sound effect
+        // Load the collect sound effect.
         _collectSoundEffect = Content.Load<SoundEffect>("audio/collect");
 
-        // Load the background theme music
-        Song theme = Content.Load<Song>("audio/theme");
-
-        // Ensure media player is not already playing on device, if so, stop it
-        if (MediaPlayer.State == MediaState.Playing)
-        {
-            MediaPlayer.Stop();
-        }
-
-        // Play the background theme music.
-        MediaPlayer.Play(theme);
-
-        // Set the theme music to repeat.
-        MediaPlayer.IsRepeating = true;
-
-        MediaPlayer.Volume = 0.25f;
+        // Load the background theme music.
+        _themeSong = Content.Load<Song>("audio/theme");
     }
 
     protected override void Update(GameTime gameTime)
@@ -127,15 +119,7 @@ public class Game1 : Core
         // Check for gamepad input and handle it.
         CheckGamePadInput();
 
-        // Create a bounding rectangle for the screen.
-        Rectangle screenBounds = new Rectangle(
-            0,
-            0,
-            GraphicsDevice.PresentationParameters.BackBufferWidth,
-            GraphicsDevice.PresentationParameters.BackBufferHeight
-        );
-
-        // Creating a bounding circle for the slime
+        // Creating a bounding circle for the slime.
         Circle slimeBounds = new Circle(
             (int)(_slimePosition.X + (_slime.Width * 0.5f)),
             (int)(_slimePosition.Y + (_slime.Height * 0.5f)),
@@ -208,32 +192,27 @@ public class Game1 : Core
             normal.Normalize();
             _batVelocity = Vector2.Reflect(_batVelocity, normal);
 
-            // Play the bounce sound effect
-            _bounceSoundEffect.Play(0.1f, 0f, 0f);
+            // Play the bounce sound effect.
+            Audio.PlaySoundEffect(_bounceSoundEffect);
         }
 
         _batPosition = newBatPosition;
 
         if (slimeBounds.Intersects(batBounds))
         {
-            // Divide the width  and height of the screen into equal columns and
-            // rows based on the width and height of the bat.
-            int totalColumns = GraphicsDevice.PresentationParameters.BackBufferWidth / (int)_bat.Width;
-            int totalRows = GraphicsDevice.PresentationParameters.BackBufferHeight / (int)_bat.Height;
-
             // Choose a random row and column based on the total number of each
-            int column = Random.Shared.Next(0, totalColumns);
-            int row = Random.Shared.Next(0, totalRows);
+            int column = Random.Shared.Next(1, _tilemap.Columns - 1);
+            int row = Random.Shared.Next(1, _tilemap.Rows - 1);
 
             // Change the bat position by setting the x and y values equal to
             // the column and row multiplied by the width and height.
             _batPosition = new Vector2(column * _bat.Width, row * _bat.Height);
 
-            // Assign a new random velocity to the bat
+            // Assign a new random velocity to the bat.
             AssignRandomBatVelocity();
 
-            // Play the collect sound effect
-            _collectSoundEffect.Play(0.1f, 0f, 0f);
+            // Play the collect sound effect.
+            Audio.PlaySoundEffect(_collectSoundEffect);
         }
     }
 
@@ -282,6 +261,26 @@ public class Game1 : Core
         if (Input.Keyboard.IsKeyDown(Keys.D) || Input.Keyboard.IsKeyDown(Keys.Right))
         {
             _slimePosition.X += speed;
+        }
+
+        // If the M key is pressed, toggle mute state for audio.
+        if (Input.Keyboard.WasKeyJustPressed(Keys.M))
+        {
+            Audio.ToggleMute();
+        }
+
+        // If the + button is pressed, increase the volume.
+        if (Input.Keyboard.WasKeyJustPressed(Keys.OemPlus))
+        {
+            Audio.SongVolume += 0.1f;
+            Audio.SoundEffectVolume += 0.1f;
+        }
+
+        // If the - button was pressed, decrease the volume.
+        if (Input.Keyboard.WasKeyJustPressed(Keys.OemMinus))
+        {
+            Audio.SongVolume -= 0.1f;
+            Audio.SoundEffectVolume -= 0.1f;
         }
     }
 
@@ -346,7 +345,7 @@ public class Game1 : Core
         // Begin the sprite batch to prepare for rendering.
         SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
-        // Draw the tilemap.
+        // Draw the tilemap
         _tilemap.Draw(SpriteBatch);
 
         // Draw the slime sprite.
